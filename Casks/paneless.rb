c581e32cba98d911389aff057b132996e26d24de
DYNNIwav <paalomei@gmail.com>
2026-02-24 15:42:42 +0100

    Update to v1.2.0

--- Casks/paneless.rb
+++ Casks/paneless.rb
@@ -1,6 +1,6 @@
 cask "paneless" do
-  version "1.1.0"
-  sha256 "d88e4b50629d2ee70c904d210f228a6955af7f6c0c784a7a3e790b34e58273d4"
+  version "1.2.0"
+  sha256 "3ab790a3b1411116e6bbeae106621d815ffba153de8b51030c680e519531b158"
 
   url "https://github.com/DYNNIwav/paneless/releases/download/v#{version}/Paneless.app.zip"
   name "Paneless"
